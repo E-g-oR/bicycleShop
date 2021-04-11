fac3313e41f7d2f7477650d92468cc8d19675024
E-g-oR <emuzych@gmail.com>
2021-04-11 11:04:34 +0300

    optimized pictures and fixed responsive

--- js/script.js
+++ js/script.js
@@ -20,6 +20,11 @@ const swiper = new Swiper('.swiper-container', {
       // when window width is >= 320px
       320: {
          spaceBetween: 30,
+         slidesPerView: 1,
+         // navigation: undefined
+      },
+      515: {
+         spaceBetween: 30,
          slidesPerView: 0.8,
          // navigation: undefined
       },
@@ -34,8 +39,29 @@ const swiper = new Swiper('.swiper-container', {
          spaceBetween: 70,
          slidesPerView: 1.5
       }
-   }
+   },
+   // on: {
+   //    init: function () {
+   //       checkArrow();
+   //    },
+   //    resize: function () {
+   //       checkArrow();
+   //    }
+   // }
 });
+
+function checkArrow() {
+   var swiperPrev = document.querySelector('.swiper-button-prev');
+   var swiperNext = document.querySelector('.swiper-button-next');
+   if (window.innerWidth > 730) {
+      console.log('Success', window.innerWidth);
+      swiperPrev.style.display = 'flex';
+      swiperNext.style.display = 'flex';
+   } else {
+      swiperPrev.style.display = 'none';
+      swiperNext.style.display = 'none';
+   }
+};
 const hamburgerMenu = document.querySelector('.hamburger-menu'),
    switchLanguage = document.querySelector('.switch-language'),
    buttons = document.querySelector('.buttons'),
